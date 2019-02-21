489f5ca932c6cb9310e9b0c193b3db1f3db3096e
Brandon <mbrandon@labs.neit.edu>
2019-02-21 15:10:06 -0500

    Feb 21 -> Added rotation and server to unity communicaitons

--- Assets/Scripts/Network.cs
+++ Assets/Scripts/Network.cs
@@ -5,8 +5,10 @@ using SocketIO;
 using System;
 
 public class Network : MonoBehaviour {
-    SocketIOComponent socket;
+    static SocketIOComponent socket;
     public GameObject playerPrefab;
+
+    Dictionary<string, GameObject> players;
 
 	// Use this for initialization
 	void Start () {
@@ -14,12 +16,32 @@ public class Network : MonoBehaviour {
         socket.On("open", OnConnected );
         socket.On("talkback", OnTalkBack);
         socket.On("spawn", OnSpawn);
+        socket.On("move", OnMove);
+
+        players = new Dictionary<string, GameObject>();
+    }
+
+    private void OnMove(SocketIOEvent obj)
+    {
+        Debug.Log("Player Moving" + obj.data);
+        var id = obj.data["id"].ToString();
+        //Debug.Log("Player ID: " + id);
+
+        var v = float.Parse(obj.data["v"].ToString().Replace("\"",""));
+        var h = float.Parse(obj.data["h"].ToString().Replace("\"", ""));
+
+        players[id].GetComponent<PlayerMovementNetwork>().v = v;
+        players[id].GetComponent<PlayerMovementNetwork>().h = h;
+       
+        
     }
 
     private void OnSpawn(SocketIOEvent obj)
     {
-        Debug.Log("Player Spawned");
-        Instantiate(playerPrefab);
+        Debug.Log("Player Spawned" + obj.data);
+        var player = Instantiate(playerPrefab);
+        players.Add(obj.data["id"].ToString(), player);
+        Debug.Log(players.Count);
     }
 
     private void OnTalkBack(SocketIOEvent obj)
@@ -31,5 +53,18 @@ public class Network : MonoBehaviour {
     {
         Debug.Log("Connected to Server");
         socket.Emit("sayhello");
+        //socket.Emit("move");
+    }
+
+    static public void Move(float currentPosV, float currentPosH)
+    {
+        Debug.Log("Send Position to Server" + VectorToJson(currentPosV, currentPosH));
+        socket.Emit("move", new JSONObject(VectorToJson(currentPosV, currentPosH)));
+        
+    }
+
+    public static string VectorToJson(float dirV, float dirH)
+    {
+        return string.Format(@"{{""v"":""{0}"",""h"":""{1}""}}", dirV, dirH);
     }
 }
